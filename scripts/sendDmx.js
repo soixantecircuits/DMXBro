6dcdd5b5d5c0d383afe0ab1084973b1da2f9c0cb
hugohil <hugo@soixantecircuits.fr>
2016-05-10 19:27:22 +0200

    Update 'send' and 'stop' test scripts

--- scripts/sendDmx.js
+++ scripts/sendDmx.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const maxChannel = require('../bin/data/config.json').maxChannel
-const interval = 5 // in seconds
 
 const spacebroClient = require('spacebro-client')
 
@@ -16,12 +15,11 @@ var actionList = [
 
 spacebroClient.registerToMaster(actionList, 'sendDmx')
 
-setInterval(function () {
+setTimeout(function () {
   for (let i = 1; i <= maxChannel; i++) {
     spacebroClient.emit('DMX-data', {channel: i, level: 255})
-    setTimeout(function () {
-      spacebroClient.emit('DMX-data', {channel: i, level: 1})
-    }, (interval * 1000) / 2)
   }
-}, (interval * 1000))
-
+  setTimeout(function () {
+    process.exit()
+  }, 100)
+}, 1000)
--- scripts/stopDmx.js
+++ scripts/stopDmx.js
@@ -1,3 +1,7 @@
+'use strict'
+
+const maxChannel = require('../bin/data/config.json').maxChannel
+
 const spacebroClient = require('spacebro-client')
 
 var actionList = [
@@ -9,15 +13,13 @@ var actionList = [
   }
 ]
 
-// spacebroClient.iKnowMyMaster('127.0.0.1', '8888')
 spacebroClient.registerToMaster(actionList, 'sendDmx')
 
 setTimeout(function () {
-  for (var i = 1; i <= 255; i++) {
-    spacebroClient.emit('DMX-data', {channel: i, level: 0})
+  for (let i = 1; i <= maxChannel; i++) {
+    spacebroClient.emit('DMX-data', {channel: i, level: 1})
   }
   setTimeout(function () {
-    console.log('stopDmx.js - I\'m done here. My people needs me. Bye.')
-    process.exit(0)
-  }, 2000)
-}, 3000)
+    process.exit()
+  }, 100)
+}, 1000)
